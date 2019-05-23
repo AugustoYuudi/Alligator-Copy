aa520f9bfe130bf5fc18f5020205d0592cdef7ad
Augusto Shiraishi <augshira@publicisgroupe.net>
2019-05-23 15:09:10 -0300

    Refatorando navbar.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,14 +1,36 @@
 <template>
     <div class="navbar">
-            <div class="navbar__logo" v-if="$route.name !== 'home'"></div>
-            <div class="navbar__explore" v-if="$route.name === 'home'">Explore</div>
-        <ul class="navbar__pages">
-            <li v-for="(pagina, index) in paginas" :key=index class="pages__page">{{pagina}}</li>
+        <ul>
+            <li class="navbar__logo" v-if="$route.name !== 'home'">
+                <a href="#" ></a>
+            </li>
+
+            <li class="navbar__explore" v-if="$route.name === 'home'">
+                <a href="#">⚡ Explore</a>
+            </li>
+
+            <li class="navbar__pages">
+                <ul>
+                    <li v-for="(pagina, index) in paginas" :key=index class="pages__page">
+                        {{pagina}}
+                    </li>
+                </ul>
+            </li>
+
+            <li class="navbar__search__container">
+                <input type="search" class="navbar__search" placeholder="しらいし">
+            </li>
+
+            <li class="navbar__menu__expander">
+                <span class="expander__dot"></span>
+                <span class="expander__dot"></span>
+                <span class="expander__dot"></span>
+            </li>
+
+            <li class="navbar__twitter">
+
+            </li>
         </ul>
-        <div class="navbar__search__container">
-            <input type="search" class="navbar__search">
-        </div>
-        <span class="navbar__last__item"></span>
     </div>
 </template>
 
@@ -16,44 +38,22 @@
 *
     margin 0
     padding 0
+    list-style none
+    text-decoration none
 
-.navbar
-    width 100vw
-    padding-top 16px
-    padding-bottom 16px
-    background-color tomato
 
-    .navbar__pages
-        display none
 
-    .pages__page, 
-    .navbar__logo,
-    .navbar__explore
-        display inline-block
+
+// @media screen and (min-width: 375px)
+
+// @media screen and (min-width: 425px)
+
+// @media screen and (min-width: 768px)
+
+// @media screen and (min-width: 1024px)
+
+// @media screen and (min-width: 1120px)
     
-    .navbar__search__container
-        background-image url('../assets/img/search.png') 
-        background-color transparent
-        background-repeat no-repeat
-        background-size 25px 25px
-        background-position 0 0.2em
-        border 1px black solid
-        border-radius 8px
-        width 130px
-        height 28px
-        margin-left 13em
-
-        .navbar__search
-            width 105px
-            height 100%
-            margin-left 25px
-            border none
-            background none
-            outline none
-
-@media screen and (min-width: 425px)
-    .navbar
-        background-color teal
 </style>
 
 <script>
